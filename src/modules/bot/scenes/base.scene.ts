910c62187174e4e8f88aa90b35fe8c774d2095a9
MyUserNameIsMy <ilyas123.822@gmail.com>
2024-03-28 12:02:45 +0500

    fix

--- src/modules/bot/scenes/base.scene.ts
+++ src/modules/bot/scenes/base.scene.ts
@@ -53,7 +53,6 @@ export class BaseScene {
         '@dan7yar': -4197036835,
         '@sherniaz16': -4167072562,
       };
-      g;
       ctx.session['curator'] = student_system?.new_curator;
       ctx.session['hm_channel'] = channels[student_system.curator];
       let direction = 'нет направления';
